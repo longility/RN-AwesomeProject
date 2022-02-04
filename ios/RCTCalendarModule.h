82ba9813bb3447a4af1ca99e90c3fbd5363f4dd5
Long Mai <long.mai@froedtert.com>
2022-02-04 12:06:39 -0600

    initial commit RN module example

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,15 @@
+//
+//  RCTCalendarModule.h
+//  AwesomeProject
+//
+//  Created by Long Mai on 2/4/22.
+//
+
+#ifndef RCTCalendarModule_h
+#define RCTCalendarModule_h
+
+#import <React/RCTBridgeModule.h>
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+@end
+
+#endif /* RCTCalendarModule_h */
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,21 @@
+//
+//  RCTCalendarModule.m
+//  AwesomeProject
+//
+//  Created by Long Mai on 2/4/22.
+//
+
+#import "RCTCalendarModule.h"
+#import <React/RCTLog.h>
+
+@implementation RCTCalendarModule
+
+// To export a module named RCTCalendarModule
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(createCalendarEvent:(NSString *)name location:(NSString *)location)
+{
+ RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
+@end
